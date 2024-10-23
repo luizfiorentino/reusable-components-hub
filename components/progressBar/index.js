950f6583ab8c29cea3f1f02520f4d716cc21ffa2
luizfiorentino <lgfiorentino@gmail.com>
2024-10-23 14:17:00 +0200

    ProgressBar displaying progress dynamically

--- components/progressBar/index.js
+++ components/progressBar/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import styles from "./index.module.css";
 
 export default function ProgressBar({
-  percentage,
+  progress,
   className,
   variant = "primary",
 }) {
   return (
     <div className={`${styles.mainContainer} ${styles[variant]}`}>
-      <div className={styles.inner}></div>
+      <div
+        className={styles.innerLeft}
+        style={progress ? { width: `${progress}%` } : {}}
+      ></div>
+      <div className={styles.innerRight}></div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ProgressBar from "@/components/progressBar";
 
 export default function index() {
+  const [progress, setProgress] = useState(50);
   return (
     <div>
       <h1>Hello mottocode</h1>
-      <ProgressBar />
+      <ProgressBar progress={progress} />
+      <button onClick={() => progress <= 90 && setProgress(progress + 10)}>
+        +
+      </button>
+      <button onClick={() => progress >= 10 && setProgress(progress - 10)}>
+        -
+      </button>
     </div>
   );
 }
