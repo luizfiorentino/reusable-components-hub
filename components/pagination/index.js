72bf56a7f9b214fe787e3d59f00ed93b64a5d9a7
luizfiorentino <lgfiorentino@gmail.com>
2024-12-04 16:18:18 +0100

    code cleaning and refactor (passing cards dynamically)

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -5,43 +5,26 @@ import Card from "../card";
 
 export default function Pagination({ arrayOfCards, cardsPerPage }) {
   const [paginationIndex, setPaginationIndex] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  cardsPerPage = 10;
 
-  const arrayOfNumbers = [];
-  for (let i = 1; i <= 101; i++) {
-    arrayOfNumbers.push(i);
-  }
-  console.log(arrayOfNumbers);
-  const totalCards = arrayOfNumbers.length;
-  const numberOfButtons = Math.ceil(arrayOfNumbers.length / cardsPerPage);
-  console.log(numberOfButtons);
+  const numberOfButtons = Math.ceil(arrayOfCards.length / cardsPerPage);
 
   const startSet = (paginationIndex - 1) * cardsPerPage;
   const endSet = startSet + 10;
-  console.log("startSet", startSet);
 
   const previousList = () => setPaginationIndex(paginationIndex - 1);
   const nextList = () => setPaginationIndex(paginationIndex + 1);
-  console.log("paginationIndex", paginationIndex);
 
   const paginationButtons = [];
   for (let i = 0; i < numberOfButtons; i++) {
-    paginationButtons.push({ index: i });
+    paginationButtons.push(i);
   }
-  console.log("paginationButtons", paginationButtons[0]["index"]);
 
   const displayInset = (index) => {
     setPaginationIndex(index + 1);
-    console.log("clicked", index);
   };
 
   return (
     <div>
-      <div className={styles.paginationContainer}>
-        <Button onClick={() => previousList()}>Previous</Button>
-        <Button onClick={() => nextList()}>Next</Button>
-      </div>
       <div className={styles.paginationContainer}>
         {paginationButtons.length &&
           paginationButtons.map((button, index) => (
@@ -53,8 +36,12 @@ export default function Pagination({ arrayOfCards, cardsPerPage }) {
             </Button>
           ))}
       </div>
+      <div className={styles.paginationContainer}>
+        <Button onClick={() => previousList()}>Previous</Button>
+        <Button onClick={() => nextList()}>Next</Button>
+      </div>
       <div className={styles.listOfCardsContainer}>
-        {arrayOfNumbers.slice(startSet, endSet).map((el, index) => (
+        {arrayOfCards.slice(startSet, endSet).map((el, index) => (
           <div className={styles.cardContainer}>
             <Card>{el}</Card>
           </div>
--- pages/pagination/index.js
+++ pages/pagination/index.js
@@ -3,9 +3,13 @@ import styles from "./index.module.css";
 import Pagination from "@/components/pagination";
 
 export default function index() {
+  const arrayOfCards = [];
+  for (let i = 1; i <= 101; i++) {
+    arrayOfCards.push(i);
+  }
   return (
     <div>
-      <Pagination />
+      <Pagination arrayOfCards={arrayOfCards} cardsPerPage={10} />
     </div>
   );
 }
