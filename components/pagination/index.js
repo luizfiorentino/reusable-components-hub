f100440e377751a87f0e2bd65f86ad7c3e9a766b
luizfiorentino <lgfiorentino@gmail.com>
2024-12-04 17:26:42 +0100

    handled sub pagination forward and back buttons logic in Pagination component

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -3,39 +3,79 @@ import styles from "./index.module.css";
 import Button from "../atoms/button";
 import Card from "../card";
 
-export default function Pagination({ arrayOfCards, cardsPerPage }) {
+export default function Pagination({
+  arrayOfCards,
+  cardsPerPage,
+  buttonsPerPage = 5,
+}) {
   const [paginationIndex, setPaginationIndex] = useState(1);
+  const [subPaginationIndex, setSubPaginationIndex] = useState(1);
 
   const numberOfButtons = Math.ceil(arrayOfCards.length / cardsPerPage);
 
   const startSet = (paginationIndex - 1) * cardsPerPage;
   const endSet = startSet + 10;
 
+  const startSet2 = (subPaginationIndex - 1) * buttonsPerPage;
+  const endSet2 = startSet2 + 5;
+
   const previousList = () => setPaginationIndex(paginationIndex - 1);
   const nextList = () => setPaginationIndex(paginationIndex + 1);
+
+  const previousSubList = () => setSubPaginationIndex(subPaginationIndex - 1);
+  const nextSubList = () => setSubPaginationIndex(subPaginationIndex + 1);
 
   const paginationButtons = [];
   for (let i = 0; i < numberOfButtons; i++) {
     paginationButtons.push(i);
   }
+  const subPaginationLastIndex = Math.ceil(
+    paginationButtons.length / buttonsPerPage
+  );
+  console.log("subPaginationLastIndex", subPaginationLastIndex);
 
   const displayInset = (index) => {
     setPaginationIndex(index + 1);
   };
-  console.log(paginationIndex);
+
+  console.log("subPaginationIndex", subPaginationIndex, startSet2, endSet2);
+  console.log("paginationButtons", paginationButtons);
 
   return (
     <div>
+      <div className={styles.listOfCardsContainer}>
+        {arrayOfCards.slice(startSet, endSet).map((el, index) => (
+          <div className={styles.cardContainer}>
+            <Card>{el}</Card>
+          </div>
+        ))}
+      </div>
       <div className={styles.paginationContainer}>
         {paginationButtons.length &&
-          paginationButtons.map((button, index) => (
+          paginationButtons.slice(startSet2, endSet2).map((button, index) => (
             <Button
               onClick={() => displayInset(index)}
               selected={index === paginationIndex - 1 ? true : false}
             >
-              {index + 1}
+              {button + 1}
             </Button>
           ))}
+      </div>
+      <div className={styles.paginationContainer}>
+        <Button
+          onClick={() => previousSubList()}
+          disabled={subPaginationIndex === 1 ? true : false}
+        >
+          {"<<"}
+        </Button>
+        <Button
+          onClick={() => nextSubList()}
+          disabled={
+            subPaginationIndex === subPaginationLastIndex ? true : false
+          }
+        >
+          {">>"}
+        </Button>
       </div>
       <div className={styles.paginationContainer}>
         <Button
@@ -50,13 +90,6 @@ export default function Pagination({ arrayOfCards, cardsPerPage }) {
         >
           Next
         </Button>
-      </div>
-      <div className={styles.listOfCardsContainer}>
-        {arrayOfCards.slice(startSet, endSet).map((el, index) => (
-          <div className={styles.cardContainer}>
-            <Card>{el}</Card>
-          </div>
-        ))}
       </div>
     </div>
   );
