bf8aede242655bfa0c9afc70455b6db6a0462975
luizfiorentino <lgfiorentino@gmail.com>
2024-12-04 15:17:12 +0100

    added numbered buttons to pagination

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -9,7 +9,7 @@ export default function Pagination({ arrayOfCards, cardsPerPage }) {
   cardsPerPage = 10;
 
   const arrayOfNumbers = [];
-  for (let i = 1; i <= 91; i++) {
+  for (let i = 1; i <= 21; i++) {
     arrayOfNumbers.push(i);
   }
   console.log(arrayOfNumbers);
@@ -17,29 +17,39 @@ export default function Pagination({ arrayOfCards, cardsPerPage }) {
   const numberOfButtons = Math.ceil(arrayOfNumbers.length / cardsPerPage);
   console.log(numberOfButtons);
 
-  const blavariable = (paginationIndex - 1) * cardsPerPage;
-  console.log("blavariable", blavariable);
+  const startSet = (paginationIndex - 1) * cardsPerPage;
+  const endSet = startSet + 10;
+  console.log("startSet", startSet);
 
   const previousList = () => setPaginationIndex(paginationIndex - 1);
   const nextList = () => setPaginationIndex(paginationIndex + 1);
   console.log("paginationIndex", paginationIndex);
 
+  const paginationButtons = [];
+  for (let i = 0; i < numberOfButtons; i++) {
+    paginationButtons.push({ index: i });
+  }
+  console.log("paginationButtons", paginationButtons[0]["index"]);
+
   return (
     <div>
-      {" "}
       <div className={styles.paginationContainer}>
         <Button onClick={() => previousList()}>Previous</Button>
         <Button onClick={() => nextList()}>Next</Button>
       </div>
-      <div className={styles.listOfCardsContainer}>
-        {arrayOfNumbers
-          .slice(blavariable, blavariable + 10)
-          .map((el, index) => (
-            <div className={styles.cardContainer}>
-              {" "}
-              <Card>{el}</Card>
-            </div>
+      <div className={styles.paginationContainer}>
+        {paginationButtons.length &&
+          paginationButtons.map((button, index) => (
+            <Button>{index + 1}</Button>
           ))}
+      </div>
+      <div className={styles.listOfCardsContainer}>
+        {arrayOfNumbers.slice(startSet, endSet).map((el, index) => (
+          <div className={styles.cardContainer}>
+            {" "}
+            <Card>{el}</Card>
+          </div>
+        ))}
       </div>
     </div>
   );
